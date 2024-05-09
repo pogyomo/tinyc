f57dd7f7fa323a2c95e57d188bc27d72fe38a410
pogyomo <abc_abc_bcd_bcd@yahoo.co.jp>
2024-05-09 12:17:40 +0900

    feat: more beautiful error message

--- src/report/report.cpp
+++ src/report/report.cpp
@@ -22,14 +22,28 @@ int digits(int n) {
     return res;
 }
 
+void start_color(std::ostream& os, ReportableLevel level) {
+    if (level == ReportableLevel::Error) {
+        os << "\e[31m";
+    } else {
+        os << "\e[33m";
+    }
+}
+
+void end_color(std::ostream& os) { os << "\e[0m"; }
+
 std::string info(const std::string& filename, const std::string& context,
                  Position start, ReportableLevel level) {
     std::stringstream ss;
     ss << filename << ":" << start.row() << ":" << start.offset() << ": ";
     if (level == ReportableLevel::Error) {
-        ss << "\e[31m" << "error: " << "\e[0m";
+        start_color(ss, level);
+        ss << "error: ";
+        end_color(ss);
     } else {
-        ss << "\e[31m" << "warning: " << "\e[0m";
+        start_color(ss, level);
+        ss << "warning: ";
+        end_color(ss);
     }
     ss << context;
     return ss.str();
@@ -42,7 +56,6 @@ void report(Context& ctx, const Reportable& r) {
     auto name = input.name();
     std::cerr << info(name, r.context(), start, r.level()) << std::endl;
     if (start.row() == end.row()) {
-        int range = end.offset() - start.offset() + 1;
         int width = digits(start.row());
         std::stringstream sep_start;
         sep_start << std::setfill('0') << std::setw(width);
@@ -54,8 +67,10 @@ void report(Context& ctx, const Reportable& r) {
         std::cerr << sep_start.str() << line_start << std::endl;
         std::cerr << sep.str();
         std::cerr << std::string(start.offset(), ' ');
-        std::cerr << std::string(range, '^');
-        std::cout << "-- " << r.what() << std::endl;
+        start_color(std::cerr, r.level());
+        std::cerr << '^' << std::string(end.offset() - start.offset(), '~');
+        end_color(std::cerr);
+        std::cout << " " << r.what() << std::endl;
     } else {
         int width = digits(std::max(start.row(), end.row()));
         std::stringstream sep_start;
@@ -74,14 +89,19 @@ void report(Context& ctx, const Reportable& r) {
         std::cerr << sep_start.str() << line_start << std::endl;
         std::cerr << sep.str();
         std::cerr << std::string(start.offset(), ' ');
-        std::cerr << std::string(line_start.size() - start.offset(), '^');
+        start_color(std::cerr, r.level());
+        std::cerr << '^';
+        std::cerr << std::string(line_start.size() - start.offset() - 1, '~');
+        end_color(std::cerr);
         std::cerr << std::endl;
         std::cerr << cont.str() << std::endl;
 
         std::cerr << sep_end.str() << line_end << std::endl;
         std::cerr << sep.str();
-        std::cerr << std::string(end.offset(), '^');
-        std::cerr << "-- " << r.what() << std::endl;
+        start_color(std::cerr, r.level());
+        std::cerr << std::string(end.offset() + 1, '~');
+        end_color(std::cerr);
+        std::cerr << " " << r.what() << std::endl;
     }
 }
 
