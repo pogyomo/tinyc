68ac16e331873c693383fa7f9cc7fc91d11dfad8
pogyomo <pogyomo@gmail.com>
2024-05-15 03:12:59 +0900

    fix: pass correct size to malloc

--- src/input/stream.c
+++ src/input/stream.c
@@ -2,13 +2,14 @@
 
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "../panic.h"
 #include "../span.h"
 #include "input.h"
 
 istream_t *istream_new(input_t *input) {
-    istream_t *is = malloc(sizeof(input_t));
+    istream_t *is = malloc(sizeof(istream_t));
     if (!is) panic_internal("failed to allocate memory");
     is->input = input;
     is->lrow = 0;
@@ -17,24 +18,31 @@ istream_t *istream_new(input_t *input) {
     return is;
 }
 
-// Returns non zero value if this stream reach to eos.
 bool istream_eos(istream_t *is) { return is->input->lines->len <= is->row; }
 
-// Returns currently peeking character.
 char istream_char(istream_t *is) {
     if (istream_eos(is))
         panic_internal("char called when istream reach to eos");
     return string_at(input_get_line(is->input, is->row), is->offset);
 }
 
-// Returns currently peeking character's position in input.
+istream_state_t istream_state(istream_t *is) {
+    istream_state_t state = {is->lrow, is->row, is->offset};
+    return state;
+}
+
+void istream_set_state(istream_t *is, istream_state_t state) {
+    is->lrow = state.lrow;
+    is->row = state.row;
+    is->offset = state.offset;
+}
+
 position_t istream_pos(istream_t *is) {
     if (istream_eos(is)) panic_internal("pos called when istream reach to eos");
     position_t pos = {is->row, is->offset};
     return pos;
 }
 
-// Advance position of this stream so that `istream_get_*` can get next item.
 void istream_advance(istream_t *is) {
     if (istream_eos(is)) return;
 
@@ -60,16 +68,7 @@ void istream_advance(istream_t *is) {
     }
 }
 
-bool istream_accept(istream_t *is, char c, position_t *end) {
-    if (istream_eos(is) || istream_char(is) != c) {
-        return false;
-    }
-    *end = istream_pos(is);
-    istream_advance(is);
-    return true;
-}
-
-bool istream_accept_str(istream_t *is, string_t *s, position_t *end) {
+bool istream_accept(istream_t *is, char *s, position_t *end) {
     if (istream_eos(is)) {
         return false;
     }
@@ -79,15 +78,16 @@ bool istream_accept_str(istream_t *is, string_t *s, position_t *end) {
     size_t offset = is->offset;
 
     position_t acc_end = istream_pos(is);
-    for (int i = 0; i < s->len; i++) {
-        if (istream_eos(is) || is->lrow != lrow ||
-            istream_char(is) != string_at(s, i)) {
+    size_t len = strlen(s);
+    for (int i = 0; i < len; i++) {
+        if (istream_eos(is) || is->lrow != lrow || istream_char(is) != s[i]) {
             is->lrow = lrow;
             is->row = row;
             is->offset = offset;
             return false;
         }
         acc_end = istream_pos(is);
+        istream_advance(is);
     }
     *end = acc_end;
     return true;
--- src/input/stream.h
+++ src/input/stream.h
@@ -7,12 +7,18 @@
 #include "../span.h"
 #include "input.h"
 
-typedef struct {
+typedef struct istream {
     input_t *input;
     size_t lrow;
     size_t row;
     size_t offset;
 } istream_t;
+
+typedef struct {
+    size_t lrow;
+    size_t row;
+    size_t offset;
+} istream_state_t;
 
 // Construct a new istream from `input`.
 istream_t *istream_new(input_t *input);
@@ -23,18 +29,22 @@ bool istream_eos(istream_t *is);
 // Returns currently peeking character.
 char istream_char(istream_t *is);
 
+// Returns internal state of this stream which can be used at
+// `istream_set_state`.
+istream_state_t istream_state(istream_t *is);
+
+// Set internal state to `state` which can be obtained from `istream_state`.
+void istream_set_state(istream_t *is, istream_state_t state);
+
 // Returns currently peeking character's position in input.
 position_t istream_pos(istream_t *is);
 
 // Advance position of this stream so that `istream_get_*` can get next item.
 void istream_advance(istream_t *is);
 
-// If current character is `c`, advance position to skip `c`, then set `end` to
-// the position of `c` in straem and return true.
-bool istream_accept(istream_t *is, char c, position_t *end);
-
 // If current characters is `s`, advance position to skip `s`, then set `end` to
 // the last character's position of `s` in straem and return true.
-bool istream_accept_str(istream_t *is, string_t *s, position_t *end);
+// Otherwise, nothing happen.
+bool istream_accept(istream_t *is, char *s, position_t *end);
 
 #endif  // TINYC_INPUT_STREAM_H_
