9c83364d8af5f3ce72b49813b8911632ff564af1
pogyomo <pogyomo@gmail.com>
2024-06-10 03:37:32 +0900

    feat: implement parser.c

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -1,3 +1,22 @@
 #include "parser.h"
 
-bool parse_file(context_t *ctx, char *path, vector_t *decls) {}
+#include "../lexer/lexer.h"
+#include "../stream.h"
+#include "ast.h"
+#include "parser_decl.h"
+#include "utils.h"
+
+bool parse_file(context_t *ctx, char *path, vector_t *decls) {
+    vector_t tokens;
+    lex_file(ctx, path, &tokens);
+
+    tstream_t ts;
+    tstream_init(&ts, &tokens);
+    decl_t decl;
+    vector_init(decls, sizeof(decl_t));
+    while (tstream_eos(&ts)) {
+        TRY(parse_decl(ctx, &ts, &decl));
+        vector_push(decls, &decl);
+    }
+    return true;
+}
--- src/parser/parser.h
+++ src/parser/parser.h
@@ -5,6 +5,7 @@
 
 // Parse given file as list of declaration and initialize `decls` with it.
 // Returns false if error happen.
+// `decls` is the vector of type `decl_t`.
 bool parse_file(context_t *ctx, char *path, vector_t *decls);
 
 #endif  // TINYC_PARSER_PARSER_H_
