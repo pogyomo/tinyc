84b2443a2e953c014275fa72b425dfd8cef9c08f
pogyomo <pogyomo@gmail.com>
2024-08-07 03:01:29 +0900

    fix: emit error to stderr

--- src/panic.c
+++ src/panic.c
@@ -5,14 +5,14 @@
 #include <stdlib.h>
 
 void fatal_error(const char *fmt, ...) {
-    fputs("\e[31mfatal error: \e[0m", stdout);
+    fputs("\e[31mfatal error: \e[0m", stderr);
 
     va_list ap;
     va_start(ap, fmt);
-    vfprintf(stdout, fmt, ap);
+    vfprintf(stderr, fmt, ap);
     va_end(ap);
 
-    fputs("\n", stdout);
+    fputs("\n", stderr);
 
     exit(EXIT_FAILURE);
 }
