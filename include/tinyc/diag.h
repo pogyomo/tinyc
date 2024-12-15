87f5f1a024f1ecd8a75ede52a09c8cfe0eb17ec2
pogyomo <pogyomo@gmail.com>
2024-12-15 19:19:33 +0900

    feat: implement simple diagnostic

---
+++ include/tinyc/diag.h
@@ -0,0 +1,48 @@
+// Copyright 2024 pogyomo
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef TINYC_DIAG_H_
+#define TINYC_DIAG_H_
+
+#include "tinyc/repo.h"
+#include "tinyc/span.h"
+#include "tinyc/string.h"
+
+enum tinyc_diag_severity {
+    TINYC_DIAG_INFO,
+    TINYC_DIAG_WARN,
+    TINYC_DIAG_ERROR,
+};
+
+/// Show diagnostic to stdout.
+void tinyc_diag(
+    enum tinyc_diag_severity severity,
+    const struct tinyc_repo *repo,
+    const struct tinyc_span *span,
+    const struct tinyc_string *what,
+    const struct tinyc_string *message
+);
+
+/// Write diagnostic to file stream.
+/// Unlike tinyc_diag, it doesn't attach any color to output.
+void tinyc_diag_fs(
+    FILE *fs,
+    enum tinyc_diag_severity severity,
+    const struct tinyc_repo *repo,
+    const struct tinyc_span *span,
+    const struct tinyc_string *what,
+    const struct tinyc_string *message
+);
+
+#endif  // TINYC_DIAG_H_
---
+++ src/diag.c
@@ -0,0 +1,214 @@
+// Copyright 2024 pogyomo
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "tinyc/diag.h"
+
+#include <assert.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "tinyc/repo.h"
+#include "tinyc/source.h"
+
+static inline void validate_span(const struct tinyc_span *span) {
+    assert(span->start.row <= span->end.row);
+    if (span->start.row == span->end.row) {
+        assert(span->start.offset <= span->end.offset);
+    }
+}
+
+static inline const char *severity_string(enum tinyc_diag_severity severity) {
+    switch (severity) {
+        case TINYC_DIAG_INFO:
+            return "info";
+        case TINYC_DIAG_WARN:
+            return "warning";
+        default:  // TINYC_DIAG_ERROR
+            return "error";
+    }
+}
+
+static inline const char *color_sequence_by_severity(
+    enum tinyc_diag_severity severity
+) {
+    switch (severity) {
+        case TINYC_DIAG_INFO:
+            return "\033[31m";
+        case TINYC_DIAG_WARN:
+            return "\033[31m";
+        default:  // TINYC_DIAG_ERROR
+            return "\033[31m";
+    }
+}
+
+static inline size_t paint(
+    FILE *fs,
+    bool color,
+    enum tinyc_diag_severity severity,
+    const char *s
+) {
+    if (color) fputs(color_sequence_by_severity(severity), fs);
+    fputs(s, fs);
+    if (color) fputs("\033[0m", fs);
+    return strlen(s);
+}
+
+static inline void emit_loc_info(
+    FILE *fs,
+    bool color,
+    const struct tinyc_source *source,
+    enum tinyc_diag_severity severity,
+    const struct tinyc_span *span,
+    const struct tinyc_string *what
+) {
+    paint(fs, color, severity, severity_string(severity));
+    fprintf(
+        fs,
+        ":%s:%ld:%ld:%ld:%ld:%s",
+        source->name.cstr,
+        span->start.row,
+        span->start.offset,
+        span->end.row,
+        span->end.offset,
+        what->cstr
+    );
+}
+
+static inline void emit_start_line(
+    FILE *fs,
+    const struct tinyc_source *source,
+    const struct tinyc_span *span,
+    const struct tinyc_string *message
+) {
+    const struct tinyc_source_line *line = tinyc_source_at(
+        source,
+        span->start.row
+    );
+    assert(line);
+
+    fprintf(fs, "%s\n", line->line.cstr);
+    for (size_t i = 0; i < span->start.offset; ++i) fputc(' ', fs);
+    for (size_t i = span->start.offset; i < line->line.len; ++i) fputc('^', fs);
+}
+
+static inline void emit_end_line(
+    FILE *fs,
+    const struct tinyc_source *source,
+    const struct tinyc_span *span,
+    const struct tinyc_string *message
+) {
+    const struct tinyc_source_line *line = tinyc_source_at(
+        source,
+        span->end.row
+    );
+    assert(line);
+
+    fprintf(fs, "%s\n", line->line.cstr);
+    for (size_t i = 0; i <= span->end.offset; ++i) fputc('^', fs);
+}
+
+static inline void emit_single_line(
+    FILE *fs,
+    const struct tinyc_source *source,
+    const struct tinyc_span *span
+) {
+    const struct tinyc_source_line *line = tinyc_source_at(
+        source,
+        span->start.row
+    );
+    assert(line);
+
+    fprintf(fs, "%s\n", line->line.cstr);
+    for (size_t i = 0; i < span->start.offset; ++i) fputc(' ', fs);
+    for (size_t i = span->start.offset; i <= span->end.offset; ++i) {
+        fputc('^', fs);
+    }
+}
+
+static inline void diagnostic_line(
+    FILE *fs,
+    bool color,
+    enum tinyc_diag_severity severity,
+    const struct tinyc_repo *repo,
+    const struct tinyc_span *span,
+    const struct tinyc_string *what,
+    const struct tinyc_string *message
+) {
+    const struct tinyc_source *source = tinyc_repo_query(repo, span->id);
+    assert(source);
+
+    emit_loc_info(fs, color, source, severity, span, what);
+    fputc('\n', fs);
+    emit_single_line(fs, source, span);
+    fprintf(fs, " %s\n", message->cstr);
+}
+
+static inline void diagnostic_lines(
+    FILE *fs,
+    bool color,
+    enum tinyc_diag_severity severity,
+    const struct tinyc_repo *repo,
+    const struct tinyc_span *span,
+    const struct tinyc_string *what,
+    const struct tinyc_string *message
+) {
+    const struct tinyc_source *source = tinyc_repo_query(repo, span->id);
+    assert(source);
+
+    emit_loc_info(fs, color, source, severity, span, what);
+    fputc('\n', fs);
+    emit_start_line(fs, source, span, message);
+    fputc('\n', fs);
+    emit_end_line(fs, source, span, message);
+    fprintf(fs, " %s\n", message->cstr);
+}
+
+static void diagnostic(
+    FILE *fs,
+    bool color,
+    enum tinyc_diag_severity severity,
+    const struct tinyc_repo *repo,
+    const struct tinyc_span *span,
+    const struct tinyc_string *what,
+    const struct tinyc_string *message
+) {
+    validate_span(span);
+    if (span->start.row == span->end.row) {
+        diagnostic_line(fs, color, severity, repo, span, what, message);
+    } else {
+        diagnostic_lines(fs, color, severity, repo, span, what, message);
+    }
+}
+
+void tinyc_diag(
+    enum tinyc_diag_severity severity,
+    const struct tinyc_repo *repo,
+    const struct tinyc_span *span,
+    const struct tinyc_string *what,
+    const struct tinyc_string *message
+) {
+    diagnostic(stdout, true, severity, repo, span, what, message);
+}
+
+void tinyc_diag_fs(
+    FILE *fs,
+    enum tinyc_diag_severity severity,
+    const struct tinyc_repo *repo,
+    const struct tinyc_span *span,
+    const struct tinyc_string *what,
+    const struct tinyc_string *message
+) {
+    diagnostic(fs, false, severity, repo, span, what, message);
+}
